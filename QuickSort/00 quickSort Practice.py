637aaeb019c928ad76fcf6f93622b5aa702d97de
woosungkim0123 <sibu2005@naver.com>
2022-04-26 08:22:38 +0900

    quick sort practice

---
+++ QuickSort/00 quickSort Practice.py
@@ -0,0 +1,35 @@
+array = [5,7,9,0,3,1,6,2,4,8] # 10개
+
+def quickSort(array, start, end):
+  if start >= end:
+    return
+  pivot = start
+  left = start + 1
+  right = end
+  
+  while left <= right:
+    while left <= end and array[left] <= array[pivot]:
+      left += 1
+    while right > start and array[right] >= array[pivot]:
+      right -= 1
+    if left > right: #엇갈렷다면 교체
+      array[right], array[pivot] = array[pivot], array[right]
+    else:
+      array[left], array[right] = array[right], array[left]
+      
+    
+    quickSort(array, start, right - 1)
+    quickSort(array, right + 1, end)
+
+
+quickSort(array, 0 , len(array) - 1)
+
+print(array)
+
+# 0 >= 9
+
+# 1 < = 9:
+#   1  <= 9 and 7 <= 5
+
+
+# 9 > 9 and 8 >= 5
