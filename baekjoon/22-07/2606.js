ece33e9508ee643c2d0786e87db810e012360099
woosungkim0123 <sibu2005@naver.com>
2022-07-24 23:11:44 +0900

    2606 resave

--- baekjoon/22-07/2606.js
+++ baekjoon/22-07/2606.js
@@ -1,10 +1,6 @@
-/*
-// 로컬에서 테스트
-
-*/
 let fs = require("fs");
-//let input = fs.readFileSync("./2606.txt").toString().split("\n");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let input = fs.readFileSync("./2606.txt").toString().split("\n");
+//let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const bfs = (graph, startNode) => {
   const visited = [];
@@ -23,14 +19,14 @@ const bfs = (graph, startNode) => {
   return visited;
 };
 
-let count = Number(input.shift());
-let edge = Number(input.shift());
-let grph = [...Array(count + 1)].map((e) => []);
+let N = Number(input.shift());
+let M = Number(input.shift());
+let grph = [...Array(N + 1)].map((e) => []);
 
-for (let i = 0; i < edge; i++) {
-  let [from, to] = input[i].split(" ").map(Number);
-  grph[from].push(to);
-  grph[to].push(from);
+for (let i = 0; i < M; i++) {
+  let [x, y] = input[i].split(" ").map(Number);
+  grph[x].push(y);
+  grph[y].push(x);
 }
 
 console.log(bfs(grph, 1).length - 1);
