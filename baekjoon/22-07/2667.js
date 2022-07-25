8ad0d9c7953b1e3c20a3f1c108e4d2db58a5bf3a
woosungkim0123 <sibu2005@naver.com>
2022-07-25 23:40:17 +0900

    2677 done

--- baekjoon/22-07/2667.js
+++ baekjoon/22-07/2667.js
@@ -15,11 +15,35 @@ for (let i = 0; i < n; i++) {
   );
 }
 console.log(squareMap);
+
 let count = 0;
+let sizeArray = [];
+let size = 0;
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+const dfs = (x, y) => {
+  squareMap[x][y] = 0;
+  size += 1;
+  for (let i = 0; i < 4; i++) {
+    let nx = x + dx[i];
+    let ny = y + dy[i];
+    if (nx >= 0 && ny >= 0 && (nx < n) & (ny < n)) {
+      if (squareMap[nx][ny] !== 0) {
+        dfs(nx, ny);
+      }
+    }
+  }
+};
+
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
-    if (squareMap[i][j] === 0) continue;
-    count += 1;
+    if (squareMap[i][j] === 1) {
+      count += 1;
+      size = 0;
+      dfs(i, j);
+      sizeArray.push(size);
+    }
   }
 }
 console.log(count);
+sizeArray.sort((a, b) => a - b).forEach((x) => console.log(x));
