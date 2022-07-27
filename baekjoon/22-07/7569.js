ce1b3b7810132415f4973e5f74a3c5464475be6c
woosungkim0123 <sibu2005@naver.com>
2022-07-27 22:50:42 +0900

    7569

--- baekjoon/22-07/7569.js
+++ baekjoon/22-07/7569.js
@@ -1,13 +1,8 @@
 let fs = require("fs");
 let input = fs.readFileSync("./7569.txt").toString().split("\n");
 //let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-/*
-const [M, N, H] = input
-  .shift()
-  .split(" ")
-  .map((v) => +v);
-*/
 
+// [x, y, z]
 const ds = [
   [-1, 0, 0],
   [1, 0, 0],
@@ -18,9 +13,7 @@ const ds = [
 ];
 const [M, N, H] = input.shift().split(" ").map(Number);
 let queue = [];
-let visit = [...Array(H)].map((h) =>
-  [...Array(N)].map((n) => Array(M).fill(0))
-);
+let visit = [...Array(H)].map(() => [...Array(N)].map(() => Array(M).fill(0)));
 let count = M * N * H;
 let z = 0;
 let answer;
@@ -28,10 +21,9 @@ let answer;
 // 초기 상태 세팅
 for (let i = 0; i < input.length; i++) {
   let box = input[i].split(" ").map(Number);
-  console.log(box);
   box.forEach((tomato, pos) => {
     if (tomato === 1) {
-      queue.push([i % N, pos, z, 0]);
+      queue.push([pos, i % N, z, 0]);
       visit[z][i % N][pos] = 1;
       count--;
     } else if (tomato === -1) {
@@ -41,3 +33,26 @@ for (let i = 0; i < input.length; i++) {
   });
   if ((i + 1) % N === 0) ++z;
 }
+
+let idx = 0;
+while (queue.length != idx) {
+  const [x, y, z, pos] = queue[idx]; // shift 쓰면 시간초과됨
+  for (let i = 0; i < ds.length; i++) {
+    const xPos = x + ds[i][0];
+    const yPos = y + ds[i][1];
+    const zPos = z + ds[i][2];
+
+    if (xPos < 0 || yPos < 0 || zPos < 0 || xPos >= M || yPos >= N || zPos >= H)
+      continue;
+    if (!visit[zPos][yPos][xPos]) {
+      visit[zPos][yPos][xPos] = 1;
+      queue.push([xPos, yPos, zPos, pos + 1]);
+      count--;
+    }
+  }
+
+  idx++;
+  answer = pos;
+}
+
+console.log(count ? -1 : answer);
