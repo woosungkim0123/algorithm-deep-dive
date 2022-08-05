201a88661f41c6f3b0fe7bec870d844d667d3640
woosungkim0123 <sibu2005@naver.com>
2022-08-06 07:15:20 +0900

    이진탐색 예제

--- notion/binary search/7-3.js
+++ notion/binary search/7-3.js
@@ -3,14 +3,33 @@ let input = fs.readFileSync("./7-3.txt").toString().trim().split("\n");
 //let [n, ...arr] = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 function binarySearch(array, target, start, end) {
-  while (start <= end) {}
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    if (array[mid] === target) {
+      return mid;
+    } else if (array[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
   return "None";
 }
 
 const n = +input.shift();
-console.log(n);
+
 const array = input[0].split(" ").map((a) => +a);
 array.sort();
-console.log(array);
 const m = +input[1];
-console.log(m);
+const x = input[2].split(" ").map((a) => +a);
+
+for (let i = 0; i < x.length; i++) {
+  let result = binarySearch(array, x[i], 0, n - 1);
+
+  if (result !== "None") {
+    process.stdout.write("yes");
+  } else {
+    process.stdout.write("no");
+  }
+  process.stdout.write(" ");
+}
