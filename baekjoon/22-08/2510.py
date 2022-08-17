c6545a137c4dc547de688ed17d30b95007921001
woosungkim0123 <sibu2005@naver.com>
2022-08-17 22:51:21 +0900

    easy 2510

---
+++ baekjoon/22-08/2510.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('2510.txt')
+
+read = sys.stdin.readline
+
+n, m = map(int, read().rstrip().split())
+
+array = []
+for i in range(1, n+1):
+    if n % i == 0:
+        array.append(i)
+
+if len(array) < m:
+    print(0)
+else:
+    print(array[m - 1])
