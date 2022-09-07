50e378c289cc1c8842df16f438484a3985081565
woosungkim0123 <sibu2005@naver.com>
2022-09-08 01:05:40 +0900

    chess complete

--- baekjoon/22-09/1018.py
+++ baekjoon/22-09/1018.py
@@ -3,6 +3,29 @@ sys.stdin = open('1018.txt')
 
 input = sys.stdin.readline
 
-a, b = map(int, input().rstrip().split())
+N = 8
+str1 = "WBWBWBWB"
+str2 = "BWBWBWBW"
+pivot1 = [str1, str2, str1, str2, str1, str2, str1, str2]
+pivot2 = [str2, str1, str2, str1, str2, str1, str2, str1]
 
-print(a, b)
+a, b = [int(x) for x in input().rstrip().split()]
+arr = [input().rstrip() for _ in range(a)]
+
+rst = 10000
+for i in range(a-N + 1):
+    for j in range(b-N + 1):
+        count = 0
+        for p in range(N):
+            for q in range(N):
+                if arr[i+p][j+q] != pivot1[p][q]:
+                    count += 1
+
+        rst = min(rst, count)
+        count = 0
+        for p in range(N):
+            for q in range(N):
+                if arr[i+p][j+q] != pivot2[p][q]:
+                    count += 1
+        rst = min(rst, count)
+print(rst)
