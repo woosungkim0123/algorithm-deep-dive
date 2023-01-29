46d49b06ca5759d53d29f3fdae52a49f336cb9e5
Woosung_Kim <sibu2005@naver.com>
2023-01-29 22:34:41 +0900

    solve using stack

---
+++ baekjoon/bk_3986.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ret;
+string s;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> n;
+	
+	while(n--) {
+		cin >> s;
+		stack<char> stk;
+	
+		for(char a : s) {
+			if(stk.size() && stk.top() == a) stk.pop();
+			else stk.push(a);
+		}
+		if(stk.size() == 0) ret++;
+	}
+	
+	cout << ret << "\n";
+	return 0; 
+}
