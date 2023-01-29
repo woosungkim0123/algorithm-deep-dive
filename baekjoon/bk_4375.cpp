ba61cfdadacf0e0806dd0a236e46d615d030b93f
Woosung_Kim <sibu2005@naver.com>
2023-01-29 23:41:46 +0900

    Modulo Operation

---
+++ baekjoon/bk_4375.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	while(scanf("%d", &n) != EOF) {
+		int cnt = 1, ret = 1;
+		while(true) {
+			if(cnt % n == 0) {
+				printf("%d\n", ret);
+				break;
+			} else {
+				cnt = (cnt * 10) + 1;
+				cnt %= n;  
+				ret++;  // 2
+			}
+		}
+	}
+}
